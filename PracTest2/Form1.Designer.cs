ffb611b5daa02de57ed7ce25d2aa312911d17913
Nepcam <cameron.nepe@gmail.com>
2020-06-01 20:13:21 +1200

    Add project files.

---
+++ PracTest2/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PracTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.textBoxUserInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDrawBars = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(720, 350);
+            this.pictureBoxDisplay.TabIndex = 0;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // textBoxUserInput
+            // 
+            this.textBoxUserInput.Location = new System.Drawing.Point(751, 50);
+            this.textBoxUserInput.Name = "textBoxUserInput";
+            this.textBoxUserInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUserInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(748, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter number of columns:";
+            // 
+            // buttonDrawBars
+            // 
+            this.buttonDrawBars.Location = new System.Drawing.Point(751, 98);
+            this.buttonDrawBars.Name = "buttonDrawBars";
+            this.buttonDrawBars.Size = new System.Drawing.Size(100, 23);
+            this.buttonDrawBars.TabIndex = 3;
+            this.buttonDrawBars.Text = "Draw Bars";
+            this.buttonDrawBars.UseVisualStyleBackColor = true;
+            this.buttonDrawBars.Click += new System.EventHandler(this.buttonDrawBars_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(751, 147);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(100, 23);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(751, 200);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 23);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 387);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDrawBars);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUserInput);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "101 Practical Test 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.TextBox textBoxUserInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDrawBars;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ PracTest2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracTest2
+{
+    public partial class Form1 : Form
+    {
+        //Name: Cameron Nepe
+        //ID:
+
+        //Width of a bar of gold or silver
+        const int BAR_WIDTH = 50;
+        //Height of a bar of gold or silver
+        const int BAR_HEIGHT = 25;
+        //The number of rows of bars to draw
+        const int NUM_ROWS = 10;
+        //The gap between rows and columns
+        const int GAP = 10;
+        //The colour for a gold bar
+        Color GOLD_BAR_COLOR = Color.Gold;
+        //The colour for a silver bar
+        Color SILVER_BAR_COLOR = Color.Silver;
+        //Row start
+        const int ROW_STARTX = 0;
+
+        //Minimum number of rows
+        const int MIN_ROWS = 2;
+        //Maximum number of rows
+        const int MAX_ROWS = 12;
+
+        //Silver bars
+        const int SILVER_BARS = 5;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Closes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Clears the picture box and textbox and sets focus to the textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            pictureBoxDisplay.Refresh();
+            textBoxUserInput.Clear();
+            textBoxUserInput.Focus();
+        }
+
+        private void buttonDrawBars_Click(object sender, EventArgs e)
+        {
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            Pen Pen1 = new Pen(Color.Black, 1);
+            SolidBrush brGold = new SolidBrush(Color.Gold);
+            SolidBrush brSilver = new SolidBrush(Color.Silver);
+            int x = 0;
+            int y = 0;
+            int numBars = 0;
+            int gapSize = GAP;
+
+            try
+            {
+                //GET how many columns of bars
+                numBars = int.Parse(textBoxUserInput.Text);
+                //CHECK if the number of rows is valid
+                if (numBars >= MIN_ROWS && numBars <= MAX_ROWS)
+                {
+                    for (int line = 1; line <= numBars; line++)
+                    {
+                        //Check the row count to set the color
+                        if (line <= SILVER_BARS)
+                        {
+                            paper.FillRectangle(brSilver, x, y, BAR_WIDTH, BAR_HEIGHT);
+                        }
+                        else
+                        {
+                            paper.FillRectangle(brGold, x, y, BAR_WIDTH, BAR_HEIGHT);
+                        }
+                        for (int i = 0; i <= numBars; i++)
+                        {                            
+                            //DRAW bar at current x and y position
+                            paper.DrawRectangle(Pen1, x, y, BAR_WIDTH, BAR_HEIGHT);
+                            //paper.FillRectangle(brGold, x, y, BAR_WIDTH, BAR_HEIGHT);
+                            //SHIFT x to the right by the width of bar + gap
+                            x += BAR_WIDTH + gapSize;
+                        }
+                        //SHIFT y down by the height of the bar + gap
+                        y += BAR_HEIGHT + gapSize;
+                        //SHIFT 
+                        x = ROW_STARTX;
+                    }
+                   
+                }
+                else
+                {
+                    //Display an error message and clear and give the focus
+                    MessageBox.Show("Please enter a value between 2 and 12 inclusive");
+                    textBoxUserInput.Clear();
+                    textBoxUserInput.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                //Display an error message and clear and give the focus
+                MessageBox.Show(ex.Message);
+                textBoxUserInput.Clear();
+                textBoxUserInput.Focus();
+            }
+        }
+    }
+}
